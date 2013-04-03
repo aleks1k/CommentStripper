57378bd859d44061582ebff1339c8578d0ae4f4f
aleks1k <aleks.kn@gmail.com>
2013-04-03 19:03:34 +0400

    filters for file types and parse optimization

--- cli.py
+++ cli.py
@@ -1,57 +1,89 @@
+from pprint import pprint
 import sys
 import os
 import itertools
+import comment_def
 
-def parseCommandLineArguments(tagDictionary = {"-r":"recursive"}, allArguments = sys.argv):
-	'''For now just allow the recursive tag, and assume every passed
-	directory should have the tag. Recursive should be -r.
-	Tags should be passed in the form of a dictionary mapping tag
-	with meaning. Output of this method should be a dictionary mapping all
-	filepaths to the results from the tagDictionary. All tags should start
-	with a '-' character, and nothing else should'''
-	directoryDictionary = {}
-	for i in range(1,len(allArguments)):
-		if allArguments[i][0] != '-':
-			mapping = ""
-			for key in tagDictionary:
-				try:
-					if allArguments[i+1] == key:
-						mapping = tagDictionary[key]
-				except IndexError:
-					#Should only get an index error while checking the
-					#last element of the provided arguments
-					print "Near The End"
-					continue
-			directoryDictionary[allArguments[i]] = mapping
-	return directoryDictionary
+def parseCommandLineArguments(tagDictionary={"-r": "recursive"}, allArguments=sys.argv):
+    '''For now just allow the recursive tag, and assume every passed
+    directory should have the tag. Recursive should be -r.
+    Tags should be passed in the form of a dictionary mapping tag
+    with meaning. Output of this method should be a dictionary mapping all
+    filepaths to the results from the tagDictionary. All tags should start
+    with a '-' character, and nothing else should'''
+    directoryDictionary = {}
+    for i in range(1, len(allArguments)):
+        if allArguments[i][0] != '-':
+            mapping = ""
+            for key in tagDictionary:
+                try:
+                    if allArguments[i + 1] == key:
+                        mapping = tagDictionary[key]
+                except IndexError:
+                    #Should only get an index error while checking the
+                    #last element of the provided arguments
+                    print "Near The End"
+                    continue
+            directoryDictionary[allArguments[i]] = mapping
+    return directoryDictionary
+
+c = comment_def.CommentDictionary()
+
+ignore_dirs = ['.git', '.idea']
+def filter_dir(dir):
+    dirname = os.path.split(dir)[-1]
+    return dirname not in ignore_dirs
 
 def getFiles(directoryDictionary):
-	'''Takes the dictionary of filepaths and tags and returns a list of 
-	all files in the associated directories'''
-	path = os.path.abspath(__file__).rstrip('cli.py')
-	allFiles = []
-	for key in directoryDictionary:
-		localPath = path + key
-		if directoryDictionary[key] == 'recursive':
-			for (current,dirs,files) in os.walk(localPath):
-				if files != []:
-					allFiles.extend(files)
-		else:
-			for (current,dirs,files) in os.walk(localPath):
-				if files != []:
-					allFiles.extend(files)
-				break
+    '''Takes the dictionary of filepaths and tags and returns a list of
+    all files in the associated directories'''
+    # path = os.path.abspath(__file__).rstrip('cli.py')
+    files_filter = []
+    allFiles = dict()
+    for exp in c.reference.keys():
+        files_filter.append(exp)
+        allFiles[exp] = []
+    print files_filter
 
-	return allFiles
-	
+    for path in directoryDictionary:
+        # localPath = path + key
+        if directoryDictionary[path] == 'recursive':
+            for (current, dirs, files) in os.walk(path):
+                dirs[:] = filter(filter_dir, dirs)
+                if files != []:
+                    for file in files:
+                        ext = os.path.splitext(file)[1][1:]
+                        if ext in files_filter:
+                            allFiles[ext].append(os.path.join(current, file))
+        # else:
+        #     for (current, dirs, files) in os.walk(path):
+        #         if files != []:
+        #             allFiles.extend(files)
+        #         break
+
+    return allFiles
+
+
 def mapFilesToTypes(allFiles):
-	filesToTypes = {}
-	for path in allFiles:
-		filesToTypes[path] = getFileEnding(path).strip().lower()
-	return filesToTypes
-	
-def getFileEnding(fileName, delimiter="."):
-	return fileName.rpartition(delimiter)
-	
+    filesToTypes = {}
+    for path in allFiles:
+        filesToTypes[path] = getFileEnding(path).strip().lower()
+    return filesToTypes
 
-print getFiles(parseCommandLineArguments(allArguments=["cli.py","..","-r","../.."]))
+
+def getFileEnding(fileName, delimiter="."):
+    return fileName.rpartition(delimiter)
+
+REPOS_PATH = "/repo/github"
+
+allFiles = getFiles(parseCommandLineArguments(allArguments=["cli.py", REPOS_PATH, "-r"]))
+
+# pprint(types_counter)
+
+results = dict()
+
+for type in allFiles:
+    for file in allFiles[type]:
+        comments = c.parseAllComments(file, type)
+        if len(comments) != 0:
+            results[file] = comments
--- comment_def.py
+++ comment_def.py
@@ -3,56 +3,85 @@
 #feedback is welcome
 import re
 
+
 class CommentDef:
     def __init__(self, blockRegex, lineRegex, blockContinuation=""):
         self.blockRegex = blockRegex
         self.lineRegex = lineRegex
         self.blockContinuation = blockContinuation
-        
-class CommentDictionary:	
-	def __init__(self):
-		cstyle = CommentDef('(/\*)(.|\r|\n)*?(?=(\*/))', '([/]{2})(.*)', "*")
-		python = CommentDef('([\']{3})([.|\r|\n]*?)(?=([\']{3}))', '(#)+(.*)')
-		perl = CommentDef('(=begin)([.|\r|\n]*?)(?=(=cut))', '(#)+(.*)')
-		ruby = CommentDef('(=begin)([.|\r|\n]*?)(?=(=end))', '(#)+(.*)')
-		lisp = CommentDef('(#|)([.|\r|\n]*?)(?=(|#))', '(;)+(.*)')
-		text = CommentDef('', '')
-		#All block comment should be indicated such that the closing
-		#element of the comment is in a lookahead assertion to allow
-		#for nested comments
 
-		self.reference = {'c': cstyle, 'cpp': cstyle, 'cs': cstyle,
-						'java': cstyle, 'js': cstyle, 'py': python, 
-						'pl': perl, 'rb': ruby, 'lisp': lisp, 'scm': lisp,
-						'txt': text}
-						
-	def parseInline(self, fileMappings):
-		'''Assumes input is the result of the mapFilesToTypes method in
-		cli.py. So long as there's a dictionary mapping file paths to file
-		types it should be fine though. Returns all supported inline comments'''
-		allComments = []
-		for key in fileMappings:
-			if fileMappings[key] in self.reference:
-				regexPattern = self.reference[key].lineRegex
-				with open(key, 'r') as corpus:
-					for line in corpus:
-						if re.match(regexPattern, line):
-							allComments.append(re.match(regexPattern, line).group(2))
-		return allComments
-		
-	def parseBlock(self, fileMappings):
-		'''Similar to parseInline, except this round looks for block comments'''
-		allComments = []
-		for key in fileMappings:
-			if fileMappings[key] in self.reference:
-				regexPattern = self.reference[key].blockRegex
-				with open(key, 'r').read() as corpus:
-					results = re.finditer(regexPattern, corpus)
-					for result in results:
-						cleanedResult = []
-						result = result.group(2).split('\n')
-						for line in result:
-							cleanedResult.append(line.strip()[len(self.reference[key].blockContinuation):].lower())
-						allComments.append(" ".join(cleanedResult))
-		return allComments
+
+class CommentDictionary:
+    def __init__(self):
+        cstyle = CommentDef('(/\*)(.|\r|\n)*?(?=(\*/))', '([/]{2})(.*)', "*")
+        python = CommentDef('([\']{3})([.|\r|\n]*?)(?=([\']{3}))', '(#)+(.*)')
+        perl = CommentDef('(=begin)([.|\r|\n]*?)(?=(=cut))', '(#)+(.*)')
+        ruby = CommentDef('(=begin)([.|\r|\n]*?)(?=(=end))', '(#)+(.*)')
+        lisp = CommentDef('(#|)([.|\r|\n]*?)(?=(|#))', '(;)+(.*)')
+        text = CommentDef('', '')
+        #All block comment should be indicated such that the closing
+        #element of the comment is in a lookahead assertion to allow
+        #for nested comments
+
+        self.reference = {'c': cstyle, 'cpp': cstyle, 'cs': cstyle,
+                          'java': cstyle, 'js': cstyle, 'py': python,
+                          'pl': perl, 'rb': ruby, 'lisp': lisp, 'scm': lisp,
+                          'txt': text}
+
+    def parseAllComments(self, path, type):
+        allComments = []
+        if type in self.reference:
+            lineRegex = re.compile(self.reference[type].lineRegex)
+            blockRegex = re.compile(self.reference[type].blockRegex)
+            with open(path, 'r') as corpus:
+                # all_file = ''
+                # parse Inline Comments
+                for line in corpus:
+                    # all_file += line
+                    res = lineRegex.match(line)
+                    if res:
+                        c = res.group(2).strip()
+                        if len(c) != 0:
+                            allComments.append(res.group(2).strip())
+
+                corpus.seek(0)
+                # parse Block Comments
+                results = blockRegex.finditer(corpus.read())
+                for result in results:
+                    if result.group(2):
+                        res = result.group(2).replace('\n', ' ').strip()
+                        if len(res) != 0:
+                            allComments.append(res)
+
+        return allComments
+
+    def parseInline(self, fileMappings):
+        '''Assumes input is the result of the mapFilesToTypes method in
+        cli.py. So long as there's a dictionary mapping file paths to file
+        types it should be fine though. Returns all supported inline comments'''
+        allComments = []
+        for key in fileMappings:
+            if fileMappings[key] in self.reference:
+                regexPattern = self.reference[key].lineRegex
+                with open(key, 'r') as corpus:
+                    for line in corpus:
+                        if re.match(regexPattern, line):
+                            allComments.append(re.match(regexPattern, line).group(2))
+        return allComments
+
+    def parseBlock(self, fileMappings):
+        '''Similar to parseInline, except this round looks for block comments'''
+        allComments = []
+        for key in fileMappings:
+            if fileMappings[key] in self.reference:
+                regexPattern = self.reference[key].blockRegex
+                with open(key, 'r').read() as corpus:
+                    results = re.finditer(regexPattern, corpus)
+                    for result in results:
+                        cleanedResult = []
+                        result = result.group(2).split('\n')
+                        for line in result:
+                            cleanedResult.append(line.strip()[len(self.reference[key].blockContinuation):].lower())
+                        allComments.append(" ".join(cleanedResult))
+        return allComments
 					
